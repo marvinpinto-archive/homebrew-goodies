536d344753f18c08529d27bc31aaf39a9a2b0b06
Marvin Pinto <me@marvinp.ca>
2014-12-29 10:39:25 -0500

    Add iTerm New Window 1.0

---
+++ Casks/iterm-new-window.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'iterm-new-window' do
+  version '1.0'
+  sha256 'e64aa3def38ffb1d223fa93a9a2c45f78d3f3e7889d904dcde6840c090d7c56d'
+
+  url 'https://www.dropbox.com/s/t9j3nq094tiluae/iterm-new-window_1.0.dmg?dl=1'
+  name 'iTerm2 New Window'
+  homepage 'https://www.google.com/chrome'
+  license :commercial
+
+  app 'iterm-new-window.app'
+end
