8945133f4b370d7a1ff119a681d284fa8c01d7ce
Marvin Pinto <me@marvinp.ca>
2014-12-29 10:38:09 -0500

    Update the dmg sha for google-chrome-incognito

--- Casks/google-chrome-incognito.rb
+++ Casks/google-chrome-incognito.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'google-chrome-incognito' do
   version '1.0'
-  sha256 'b7bed98cf723d746c8e722e9767681cff8d0b0ac5027de57aecf1321eace7667'
+  sha256 'a6e93d7d722dc4e33abacf41cbb45b5a687331460a3b565e95c9c3fbd6e06697'
 
   url 'https://www.dropbox.com/s/5vs877a1y39osrb/chrome-incognito_1.0.dmg?dl=1'
   name 'Google Chrome Incognito'
